d4a0870a91c630e94ec55b424d1d5013b39acf76
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-08-24 10:32:22 +0100

    correction du test model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
-describe 'Task', type: :model do
-  describe 'Task Model test' do
+RSpec.describe Task, type: :model do
+  describe 'Task model test' do
     context 'Si le Title la tâche est vide' do
-      it "C'est difficile à Validé" do
-        task = Task.new(name: '', content: 'content test')
+      it "C'est difficile à Validation" do
+        task = Task.new(name: '', content: '失敗test')
         expect(task).not_to be_valid
       end
     end
   end
-  describe "Task Model test" do
-    context "Si les détails de la tâche sont vides" do
-      it "validation interceptés" do
-        
+  describe "fonction de validation taches vides" do
+    context 'Si les détails de la tâche sont vides' do
+      it 'Validation est intercepté' do
+          # Écrivez du contenu ici
       end
     end
   end
-  describe "Task model Test" do
-    context "sile titre et les détails sont décrits" do
-      it "validation passes" do
-        
+    
+  describe "fonction de validation des taches décrites" do
+    context 'Si le Title et les détails de la tâche sont décrits' do
+      it 'Validation passes' do
+        # Écrivez du contenu ici
       end
     end
   end
-
 end
