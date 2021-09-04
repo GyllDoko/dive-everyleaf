294e16ad6d86e7895e174258ad5ad3f1a0c40a5e
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-09-04 21:45:48 +0100

    add step3 requirements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_205356) do
+ActiveRecord::Schema.define(version: 2021_09_03_193213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_09_01_205356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "expiry_date"
+    t.integer "status"
+    t.integer "priority"
   end
 
 end
